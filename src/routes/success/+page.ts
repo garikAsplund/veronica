c77272c1751c3bac6b495a21ca8266acd8fdb0f6
Garik Asplund <garik.asplund@gmail.com>
2023-11-16 18:36:22 -0800

    safety first

--- src/routes/success/+page.ts
+++ src/routes/success/+page.ts
@@ -3,7 +3,7 @@ import { stripe } from '../stripe';
 
 export const load: PageLoad = async ({ url }) => {
 	const session = await stripe.checkout.sessions.retrieve(url.searchParams.get('session_id'));
-	const customerName = session.customer_details.name ? session.customer_details.name : '';
+	const customerName = session.customer_details.name ? session.customer_details.name.split(' ')[0] : '';
 
 	return {
 		customerName
